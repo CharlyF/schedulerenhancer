1af40d32f054e94f486f548bd083c80f97bb6163
charlyF <charly@datadoghq.com>
2020-03-19 22:55:21 -0400

    boilerplate controller

---
+++ pkg/apis/datadoghq/group.go
@@ -0,0 +1,6 @@
+// Package datadoghq contains datadoghq API versions.
+//
+// This file ensures Go source parsers acknowledge the datadoghq package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package datadoghq
